8c24f2ea3e2ec6eb40642f10e6b84e0bf64e96e0
Assaf Giladi <assaf@weka.io>
2026-05-13 14:59:30 +0300

    fix: use GitHub Release assets instead of gh-pages

--- Formula/wekakube.rb
+++ Formula/wekakube.rb
@@ -6,11 +6,11 @@ class Wekakube < Formula
 
   on_macos do
     on_arm do
-      url "https://weka.github.io/wekakube/releases/wekakube_1.0.0_darwin_arm64.tar.gz"
+      url "https://github.com/weka/wekakube/releases/download/v1.0.0/wekakube_1.0.0_darwin_arm64.tar.gz"
       sha256 "ad912cb914f34af9856b54b1ac8a1adc25290adf0c6bcf4df78ec16523eb7eaf"
     end
     on_intel do
-      url "https://weka.github.io/wekakube/releases/wekakube_1.0.0_darwin_amd64.tar.gz"
+      url "https://github.com/weka/wekakube/releases/download/v1.0.0/wekakube_1.0.0_darwin_amd64.tar.gz"
       sha256 "e48a8c4d2b73ac8307045a79dbda053c2c6421d1f63f57ec9e0e0cd051f9ab17"
     end
   end
